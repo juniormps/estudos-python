342ea98c3e8a4a0a100e6725abc39849a34193d3
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-16 11:34:31 -0300

    Testes iniciais Python

---
+++ 1_fundamentos/teste.py
@@ -0,0 +1,16 @@
+# Para executar um código Python, deve-se pressionar as teclas "ctrl + alt + n"
+
+print("Olá Python!")
+
+a: int = 2
+b: int = 5
+
+print(a, "e", b)
+
+a, b = b, a
+
+print(a, "e", b)
+
+c: int = 10
+
+print(c)
