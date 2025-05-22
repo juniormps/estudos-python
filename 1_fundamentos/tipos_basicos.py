7801a2f8f20ada1c651cc8c93541200f39c0e1ed
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-22 16:56:53 -0300

    tipos básicos

---
+++ 1_fundamentos/tipos_basicos.py
@@ -0,0 +1,23 @@
+#Boolean
+#(sempre com a inicial maiúscula "True" ou "False")
+print(True)
+print(False)
+
+#Float (real) e Int (inteiro)
+print(1.2 + 1)
+
+#String
+print('Aqui eu falo na minha lingua!')
+print("Tb funciona.")
+print('Você é ' + 3 * 'muito ' + 'legal!')
+
+# print(3 + '3') -> ambiguidade (um inteiro + uma string)
+
+#Lista (array)
+print([1, 2, 3])
+
+#Dicionário (Objeto)
+print({'nome': 'Pedro', 'idade': 22})
+
+#None (null / undefined)
+print(None)
