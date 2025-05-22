90d0f07ddd35deca6fd345304489a3d4caf9c027
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-05-22 17:59:45 -0300

    Create comentarios.py

---
+++ 1_fundamentos/comentarios.py
@@ -0,0 +1,20 @@
+# Minhas variáveis
+salario = 3450.45
+despesas = 2456.2
+
+"""
+A ideia é calcular o
+quanto vai sobrar no
+final do mês!
+"""
+print(salario - despesas)
+
+# print('Fim')
+print('Fim de verdade')  # comentário aqui tb vale!
+
+'''
+A ideia é calcular o
+quanto vai sobrar no
+final do mês!
+'''
+print('Agora sim o fim de verdade')
