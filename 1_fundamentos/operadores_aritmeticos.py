57f359f2f454bee5a11fb9e3504f1c5b1cff35a5
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-06-02 18:44:30 -0300

    Create operadores_aritmeticos.py

---
+++ 1_fundamentos/operadores_aritmeticos.py
@@ -0,0 +1,13 @@
+# Operadores Aritméticos
+
+print(2 + 3)
+print(4 - 7)
+print(2 * 5.3)
+print(9.4 / 3)  #Divisão com resultado real
+print(9.4 // 3)  # Divisão com resultado inteiro (arredontado)
+print(2 ** 8)
+print(10 % 3)  #Módulo da divisão (resto)
+
+a = 12
+b = a
+print(a + b)
