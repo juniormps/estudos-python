f2052af88fa8ba6e83e93b14df7ad4f287a19f8e
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-06-02 19:03:29 -0300

    Create operadores_relacionais.py

---
+++ 1_fundamentos/operadores_relacionais.py
@@ -0,0 +1,9 @@
+# Operadores Relacionais
+
+print(3 > 4)  #maior
+print(4 >= 3)  #maior ou igual
+print(1 < 2)  #menor
+print(3 <= 1)  #menor ou igual
+print(3 != 2)  #diferente
+print(3 == 3)  #igual
+print(2 == '2') 
